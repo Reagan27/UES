c0ee6efbb2b71b69fd852e08578e1471dabf01a2
Reagan27 <luttareagan27@gmail.com>
2024-02-08 00:50:20 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Accord.Video.FFMPEG;
+  
+
+namespace UES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void CreateVideoFromImages(string[] imagePaths, string outputVideoPath)
+        {
+            int width = 1280;
+            int height = 720;  
+            int frameRate = 10; 
+
+            VideoFileWriter writer = new VideoFileWriter();
+            writer.Open(outputVideoPath, width, height, frameRate, VideoCodec.MPEG4);
+
+            foreach (var imagePath in imagePaths)
+            {
+                Bitmap image = new Bitmap(imagePath);
+                writer.WriteVideoFrame(image);
+                image.Dispose();
+            }
+
+            writer.Close();
+        }
+    }
+}
